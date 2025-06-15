5bddd0db60f70a7b8655126813eceb0895e219f4
Lochard <110186560+locharp@users.noreply.github.com>
2025-06-15 23:32:21 +0100

    Create 8. 二维费用的背包问题.java

---
+++ AcWing/8. 二维费用的背包问题.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+
+
+
+
+
+class Main
+{
+    
+    public static
+    void
+    main
+    (
+        String[] args
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        int N = read.nextInt();
+        int V = read.nextInt();
+        int M = read.nextInt();
+        int[][] a = new int[V + 1][M + 1];
+        
+        
+        for
+        (
+            int i = 0
+            ; i < N
+            ; i++
+        )
+        {
+            int v = read.nextInt();
+            int m = read.nextInt();
+            int w = read.nextInt();
+            
+            
+            for
+            (
+                int j = V
+                ; j >= v
+                ; j--
+            )
+            {
+                
+                
+                for
+                (
+                    int k = M
+                    ; k >= m
+                    ; k--
+                )
+                {
+                    a[j][k] = Math.max( a[j][k], w + a[j - v][k - m] );
+                }
+                
+                
+            }
+            
+        }
+        
+        
+        System.out.println( a[V][M] );
+    }
+    
+    
+}
