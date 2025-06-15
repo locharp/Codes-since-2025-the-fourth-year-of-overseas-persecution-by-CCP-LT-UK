06e6e9c4044932a375d570b4b0c5ae17b760a7cd
Lochard <110186560+locharp@users.noreply.github.com>
2025-06-15 19:59:14 +0100

    Create 3. 完全背包问题.java

---
+++ AcWing/3. 完全背包问题.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+
+
+
+
+class Main
+{
+    
+    public static
+    void
+    main
+    (
+        String[] args
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        int N = read.nextInt();
+        int V = read.nextInt();
+        int[] v = new int[N];
+        int[] w = new int[N];
+        int[] a = new int[V + 1];
+        
+        
+        for
+        (
+            int i = 0
+            ; i < N
+            ; i++
+        )
+        {
+            v[i] = read.nextInt();
+            w[i] = read.nextInt();
+        }
+        
+        
+        for
+        (
+            int i = 0
+            ; i < N
+            ; i++
+        )
+        {
+            
+            
+            for
+            (
+                int j = v[i]
+                ; j <= V
+                ; j++
+            )
+            {
+                a[j] = Math.max( a[j], w[i] + a[j - v[i]] );
+            }
+            
+            
+        }
+        
+        
+        System.out.println( a[V] );
+    }
+    
+}
