c4e82b7b3b41c74d810ce1c2eaab93c43115b510
Lochard <110186560+locharp@users.noreply.github.com>
2025-06-17 20:24:11 +0100

    Update 1. Total Salary.java

--- Coding Ninjas/Challenges/6-Month Code Marathon - 1/1. Total Salary.java
+++ Coding Ninjas/Challenges/6-Month Code Marathon - 1/1. Total Salary.java
@@ -7,38 +7,38 @@ import java.util.*;
 public class Solution
 {
 
-	public static
-	int
-	totalSalary
-	(
-		int basic
-		, char grade
-	)
-	{
-		double ans = basic * 1.59;
-		
+    public static
+    int
+    totalSalary
+    (
+        int basic
+        , char grade
+    )
+    {
+        double ans = basic * 1.59;
+        
 
-		if
-		(
-			grade == 'A'
-		)
-		{
-			ans += 1700;
-		}
-		else if
-		(
-			grade == 'B'
-		)
-		{
-			ans += 1500;
-		}
-		else
-		{
-			ans += 1300;
-		}
+        if
+        (
+            grade == 'A'
+        )
+        {
+            ans += 1700;
+        }
+        else if
+        (
+            grade == 'B'
+        )
+        {
+            ans += 1500;
+        }
+        else
+        {
+            ans += 1300;
+        }
 
 
-		return ( int ) ( Math.round( ans ) );
-	}
+        return ( int ) ( Math.round( ans ) );
+    }
 
 }
