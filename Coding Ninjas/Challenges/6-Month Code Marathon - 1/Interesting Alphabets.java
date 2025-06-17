7ce1daf023952133eb1ad9f157ddc8d2067e219b
Lochard <110186560+locharp@users.noreply.github.com>
2025-06-17 20:13:50 +0100

    Create Interesting Alphabets.java

---
+++ Coding Ninjas/Challenges/6-Month Code Marathon - 1/Interesting Alphabets.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+
+
+
+public class Solution
+{
+
+	public static
+	ArrayList< ArrayList< Character > >
+	interestingPattern
+	(
+		int N
+	)
+	{
+		ArrayList< ArrayList< Character > > ans = new ArrayList<>();
+
+
+		for
+		(
+			int i = 1
+			; i <= N
+			; i ++
+		)
+		{
+			ArrayList< Character > list = new ArrayList<>();
+
+
+			for
+			(
+				int j = N - i
+				; j < N
+				; j ++
+			)
+			{
+				list.add( ( char ) ( 'A' + j ) );
+			}
+
+
+			ans.add( list );
+		}
+
+
+		return ans;
+	}
+
+}
