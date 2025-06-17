092e180529eccd47a5f2a8bb810ebce9d64c1eaf
Lochard <110186560+locharp@users.noreply.github.com>
2025-06-17 20:23:50 +0100

    Update 2. Interesting Alphabets.java

--- Coding Ninjas/Challenges/6-Month Code Marathon - 1/2. Interesting Alphabets.java
+++ Coding Ninjas/Challenges/6-Month Code Marathon - 1/2. Interesting Alphabets.java
@@ -7,42 +7,42 @@ import java.util.*;
 public class Solution
 {
 
-	public static
-	ArrayList< ArrayList< Character > >
-	interestingPattern
-	(
-		int N
-	)
-	{
-		ArrayList< ArrayList< Character > > ans = new ArrayList<>();
+    public static
+    ArrayList< ArrayList< Character > >
+    interestingPattern
+    (
+        int N
+    )
+    {
+        ArrayList< ArrayList< Character > > ans = new ArrayList<>();
 
 
-		for
-		(
-			int i = 1
-			; i <= N
-			; i ++
-		)
-		{
-			ArrayList< Character > list = new ArrayList<>();
+        for
+        (
+            int i = 1
+            ; i <= N
+            ; i ++
+        )
+        {
+            ArrayList< Character > list = new ArrayList<>();
 
 
-			for
-			(
-				int j = N - i
-				; j < N
-				; j ++
-			)
-			{
-				list.add( ( char ) ( 'A' + j ) );
-			}
+            for
+            (
+                int j = N - i
+                ; j < N
+                ; j ++
+            )
+            {
+                list.add( ( char ) ( 'A' + j ) );
+            }
 
 
-			ans.add( list );
-		}
+            ans.add( list );
+        }
 
 
-		return ans;
-	}
+        return ans;
+    }
 
 }
