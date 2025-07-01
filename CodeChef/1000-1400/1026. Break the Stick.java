ead95c8b6a2fa5aee8051ed75d8aeeed135aeb34
Lochard <110186560+locharp@users.noreply.github.com>
2025-07-01 10:30:29 +0100

    Create 1026. Break the Stick.java

---
+++ CodeChef/1000-1400/1026. Break the Stick.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+
+
+
+class Codechef
+{
+
+    public static
+    void
+    main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int T = read.nextInt();
+
+
+        for (int h = 0; h < T; h++)
+        {
+            int N = read.nextInt();
+            int X = read.nextInt();
+
+
+            if (
+                (
+                    X % 2 < 1
+                    &&
+                    (
+                        N % 2 > 0
+                        || X + 2 > N
+                    )
+                )
+                ||
+                X + 1 + ( N % 2 ) > N
+            )
+            {
+                System.out.println("NO");
+            }
+            else
+            {
+                System.out.println("YES");
+            }
+            
+        }
+        
+
+    }
+
+}
