dadbf1676b3e3be13f038655be74369e67457490
Lochard <110186560+locharp@users.noreply.github.com>
2025-07-01 10:47:53 +0100

    Create Positive and Negative.java

---
+++ CodeChef/Positive and Negative.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+
+
+
+class Codechef
+{
+
+    public static
+    void
+    main
+    (
+        String[] args
+    )
+    {
+        Scanner read = new Scanner( System.in );
+        
+        
+        for
+        (
+            int i = 0
+            ; i < 3
+            ; i ++
+        )
+        {
+            int num = read.nextInt();
+            
+            if
+            (
+                num > 0
+            )
+            {
+                System.out.println("Positive");
+            }
+            else if
+            (
+                num < 0
+            )
+            {
+                System.out.println("Negative");
+            }
+            else
+            {
+                System.out.println("Zero");
+            }
+            
+        }
+        
+        
+    }
+
+}
