1e4f747d30c852c9da9da478800007f6592d51f4
Lochard <110186560+locharp@users.noreply.github.com>
2025-07-01 11:17:03 +0100

    Create Sugarcane Juice Business.java

---
+++ CodeChef/500-1000/Sugarcane Juice Business.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+
+
+
+class Codechef
+{
+
+    public static
+    void
+    main(String[] args)
+    {
+        Scanner read = new Scanner(System.in);
+        int T = read.nextInt();
+
+
+        for
+        (
+            int h = 0
+            ; h < T
+            ; h ++
+        )
+        {
+            System.out.println( read.nextInt() * 15 );
+        }
+        
+        
+    }
+
+}
