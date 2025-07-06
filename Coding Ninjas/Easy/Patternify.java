056860fbddb116f14d043913907357321a9dccce
Lochard <110186560+locharp@users.noreply.github.com>
2025-07-06 19:33:55 +0100

    Create Patternify.java

---
+++ Coding Ninjas/Easy/Patternify.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+
+
+
+public class Solution
+{
+
+    public static
+    String[]
+    printPatt
+    (
+        int n
+    )
+    {
+        String[] ans = new String[n];
+
+
+        for
+        (
+            int i = 0
+            ; i < n
+            ; i ++
+        )
+        {
+            ans[i] = "*".repeat( n - i );
+        }
+
+
+        return ans;
+    }
+
+}
