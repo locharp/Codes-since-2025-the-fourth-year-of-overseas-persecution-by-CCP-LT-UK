2bd5c7c7fa4cb0513970525f6a4bf6407193d4a1
Lochard <110186560+locharp@users.noreply.github.com>
2025-07-19 13:58:42 +0100

    Create 15. Convert To Lower Case.java

---
+++ Coding Ninjas/Challenges/6-Month Code Marathon - 1/15. Convert To Lower Case.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+
+
+
+public class Solution
+{
+
+    public static
+    String
+    toLowerCase
+    (
+        String str
+    )
+    {
+        return str.toLowerCase();
+    }
+
+}
