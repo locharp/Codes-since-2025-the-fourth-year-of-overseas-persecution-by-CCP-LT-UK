354089844c90d608bd8615f7ba5fc30ce5edbaed
Lochard <110186560+locharp@users.noreply.github.com>
2025-07-19 17:50:11 +0100

    Update and rename 5. String Sort.py to 5. String Sort.java

---
+++ Coding Ninjas/Challenges/6-Month Code Marathon - 1/5. String Sort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+
+
+
+
+public class Solution
+{
+	public static
+	String
+	stringSort
+	(
+		String s
+	)
+	{
+		char[] arr = s.toCharArray();
+
+
+		Arrays.sort( arr );
+		StringBuilder ans = new StringBuilder();
+
+
+		for
+		(
+			char c
+			: arr
+		)
+		{
+			ans.append( c );
+		}
+
+
+		return ans.toString();
+	}
+
+}
--- Coding Ninjas/Challenges/6-Month Code Marathon - 1/5. String Sort.py
+++
@@ -1,4 +0,0 @@
-def stringSort( s ):
-    
-
-    return "".join( sorted( s ) )
