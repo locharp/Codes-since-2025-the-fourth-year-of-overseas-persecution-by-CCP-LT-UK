4395e6e752cf55071b739c8b86eaba27d059ac7f
Lochard <110186560+locharp@users.noreply.github.com>
2025-08-05 12:20:44 +0100

    Create Chef and Brain Speed.java

---
+++ CodeChef/Practice/Chef and Brain Speed.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+
+
+
+class Codechef
+{
+
+    public static
+    void
+    main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        if (y > x) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+
+}
