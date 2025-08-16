a4c7b53a63890de92f4a69df543795994f9db798
Lochard <110186560+locharp@users.noreply.github.com>
2025-08-16 14:47:51 +0100

    Create Print Squares.java

---
+++ CodeChef/Practice/Print Squares.java
@@ -0,0 +1,18 @@
+class Codechef
+{
+
+    public static void main(String[] args)
+    {
+        int num = 1;
+
+
+        while (num < 6)
+        {
+            System.out.print((num * num)+ " ");
+            num++;
+        }
+        
+        
+    }
+
+}
