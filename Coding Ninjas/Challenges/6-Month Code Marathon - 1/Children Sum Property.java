c5b67d12a653b7d263ee009ede19fccafe7b8c3d
Lochard <110186560+locharp@users.noreply.github.com>
2025-09-02 18:07:31 +0100

    Create Children Sum Property.java

---
+++ Coding Ninjas/Challenges/6-Month Code Marathon - 1/Children Sum Property.java
@@ -0,0 +1,97 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node left;
+     public Node right;
+
+     Node()
+     {
+         this.data = 0;
+         this.left = null;
+         this.right = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.left = null;
+         this.right = null;
+     }
+
+     Node(int data, Node left, Node right)
+     {
+         this.data = data;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+ *****************************************************************/
+public class Solution
+{
+
+    public static
+    boolean
+    isParentSum
+    (
+        Node root
+    )
+    {
+
+        if
+        (
+            root == null
+        )
+        {
+            return true;
+        }
+        else if
+        (
+            root.left == null
+        )
+        {
+
+            if
+            (
+                root.right == null
+            )
+            {
+                return true;
+            }
+            else
+            {
+
+                return
+                isParentSum( root.right )
+                && root.data == root.right.data;
+            }
+
+        }
+        else
+        {
+
+            if
+            (
+                root.right == null
+            )
+            {
+                return
+                isParentSum( root.left )
+                && root.data ==  root.left.data;
+            }
+            else
+            {
+                return
+                isParentSum( root.left )
+                && isParentSum( root.right )
+                && root.data == root.left.data + root.right.data;
+            }
+
+        }
+
+    }
+
+}
