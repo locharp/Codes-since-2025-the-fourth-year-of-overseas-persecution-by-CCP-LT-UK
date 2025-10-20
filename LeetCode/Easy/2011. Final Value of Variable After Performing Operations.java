235fe28a9791a869bf1e36e1023579fbdcb94c11
Lochard <110186560+locharp@users.noreply.github.com>
2025-10-21 00:13:04 +0100

    Implement finalValueAfterOperations method

---
+++ LeetCode/Easy/2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,39 @@
+class Solution
+{
+
+    public
+    int
+    finalValueAfterOperations
+    (
+        String[] operations
+    )
+    {
+        int ans = 0;
+
+
+        for
+        (
+            String operation
+            : operations
+        )
+        {
+
+            if
+            (
+                operation.charAt( 1 ) == '+'
+            )
+            {
+                ans ++;
+            }
+            else
+            {
+                ans --;
+            }
+
+        }
+
+
+        return ans;
+    }
+
+}
