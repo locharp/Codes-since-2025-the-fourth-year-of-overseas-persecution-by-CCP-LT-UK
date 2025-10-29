db0ba7aac3c062f87ac7d355a1f927de54f8ac33
Lochard <110186560+locharp@users.noreply.github.com>
2025-10-29 21:20:55 +0000

    Add solution for smallest number with all set bits

---
+++ LeetCode/Easy/3370. Smallest Number With All Set Bits2.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public
+    int
+    smallestNumber
+    (
+        int n
+    )
+    {
+        return Integer.valueOf
+        (
+            "1".repeat( Integer.toBinaryString( n ).length() )
+            , 2
+        );
+    }
+
+}
